a051a49fd37afeca3bcb66bd4e78dbdb139dbf47
Jeremy Muhlich <jmuhlich@bitflood.org>
2018-09-04 23:23:41 -0400

    Properly center stage position error
    This also required adding padding in Camera when the imaged region
    falls partially outside the reference image.

--- models.py
+++ models.py
@@ -19,8 +19,8 @@ class Stage:
         return np.array([self.x, self.y])
 
     def goto(self, x, y):
-        self.x = x + np.random.rand() * 30
-        self.y = y + np.random.rand() * 30
+        self.x = x + np.random.rand() * 30 - 15
+        self.y = y + np.random.rand() * 30 - 15
 
 
 
@@ -48,12 +48,30 @@ class Camera:
         else:
             raise ValueError("Unhandled dtype")
 
-    def acquire(self):
-        lower = self.stage.position / self.scale
+    def acquire(self, position=None):
+        if position is None:
+            position = self.stage.position
+        lower = np.array(position) / self.scale
         upper = lower + [self.width, self.height]
-        x1, y1 = lower.astype(int)
-        x2, y2 = upper.astype(int)
+        x1, y1 = np.floor(lower).astype(int)
+        x2, y2 = np.floor(upper).astype(int)
+        pad_left = pad_right = pad_top = pad_bottom = 0
+        if x1 < 0:
+            pad_left = -x1
+            x1 = 0
+        if y1 < 0:
+            pad_top = -y1
+            y1 = 0
+        if x2 >= self.image.shape[1]:
+            pad_right = x2 - self.image.shape[1] + 1
+            x2 = self.image.shape[1]
+        if y2 >= self.image.shape[0]:
+            pad_right = y2 - self.image.shape[0] + 1
+            y2 = self.image.shape[0]
+        padding = ((pad_top, pad_bottom), (pad_left, pad_right))
         a = self.image[y1:y2, x1:x2]
+        a = np.pad(a, padding, 'constant')
+        assert a.shape == (self.height, self.width)
         return a
 
 
